6bf1e0e9f1f61949d22c3d4e4888ba118cbb3fbe
Badmus <badmus@Badmuss-MacBook-Pro.local>
2018-11-23 04:01:04 +0000

    initial commit

---
+++ MvcApplication/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MvcApplication.Entities
+{
+    public class User
+    {
+        public User(): IdentityUser
+        {
+        }
+    }
+}
---
+++ MvcApplication/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OdeToFood.Entities;
+using System.Collections.Generic;
+
+namespace MvcApplication.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public string CurrentMessage { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+    }
+}
